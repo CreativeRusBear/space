5c5848e4065c39baf95b65707b25fa7e942fee66
Artem Gusev <gusev2014russia@mail.ru>
2020-12-21 22:57:28 +0300

    linting files by using eslint

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,19 +1,17 @@
 module.exports = {
-    ci: {
-      collect: {
-        /* Add configuration here */
-        staticDistDir: './dist',
-        url: ['http://localhost:8080']
-      },
-      assert: {
-        assertions: {
-          'categories:performance': ['warn', {minScore: 1}],
-          'categories:accessibility': ['error', {minScore: 1}]
-        }
-      },
-      upload: {
-        /* Add configuration here */
-        target: 'temporary-public-storage',
-      },
-    },
-  };
+	ci: {
+		collect: {
+			staticDistDir : './dist',
+			url           : [ 'http://localhost:8080' ],
+		},
+		assert: {
+			assertions: {
+				'categories:performance'   : [ 'warn', {minScore: 1} ],
+				'categories:accessibility' : [ 'error', {minScore: 1} ],
+			},
+		},
+		upload: {
+			target: 'temporary-public-storage',
+		},
+	},
+};
